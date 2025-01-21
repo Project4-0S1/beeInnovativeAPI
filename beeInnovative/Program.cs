6441726a67763c1295200de318507577c2036893
Jorrit Geurts <r0956231@student.thomasmore.be>
2025-01-21 15:10:48 +0100

    BackEnd Upgrade

--- beeInnovative/Program.cs
+++ beeInnovative/Program.cs
@@ -3,14 +3,24 @@ using beeInnovative.DAL;
 using beeInnovative.DAL.Data;
 using beeInnovative.DAL.Service;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString
     = builder.Configuration.GetConnectionString("DefaultConnection");
-    builder.Services.AddDbContext<BeeInnovativeContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<BeeInnovativeContext>(options =>
+options.UseSqlServer(connectionString));
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:4200", "https://beeinnovative.netlify.app") // Allow your Angular app
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -22,15 +32,26 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "BeeInnovative",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
+app.UseSwagger();
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
+if (app.Environment.IsDevelopment())
+{
     app.UseSwaggerUI();
-//}
+}
+
+// Use CORS
+app.UseCors("AllowSpecificOrigin");
 
 using (var scope = app.Services.CreateScope())
 {

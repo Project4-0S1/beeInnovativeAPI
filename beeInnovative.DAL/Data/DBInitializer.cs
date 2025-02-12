6ffbc14350d676496e351a47bc12032f8664019d
Marnick Goossens <77274498+MarnickGoossens@users.noreply.github.com>
2025-02-12 10:02:40 +0100

    fix

--- beeInnovative.DAL/Data/DBInitializer.cs
+++ beeInnovative.DAL/Data/DBInitializer.cs
@@ -39,28 +39,8 @@ namespace beeInnovative.DAL.Data
                 IotId = "2",
             };
 
-            Beehive b3 = new Beehive()
-            {
-                //Id = 1,
-                BeehiveName = "",
-                Latitude = 51.161736776231265,
-                Longitude = 5.003174937424427,
-                IotId = "3",
-            };
-
-            Beehive b4 = new Beehive()
-            {
-                //Id = 2,
-                BeehiveName = "",
-                Latitude = 51.134813494164845,
-                Longitude = 4.941647521836946,
-                IotId = "4",
-            };
-
             context.Add(b1);
             context.Add(b2);
-            context.Add(b3);
-            context.Add(b4);
 
             //Add Colors
             Color c1 = new Color()
@@ -75,8 +55,23 @@ namespace beeInnovative.DAL.Data
                 ColorName = "Blue",
             };
 
+            Color c3 = new Color()
+            {
+                //Id = 2,
+                ColorName = "Green",
+            };
+
+            Color c4 = new Color()
+            {
+                //Id = 2,
+                ColorName = "Unknown",
+            };
+
             context.Add(c1);
             context.Add(c2);
+            context.Add(c3);
+            context.Add(c4);
+
 
             Status s2 = new Status()
             {
@@ -102,55 +97,30 @@ namespace beeInnovative.DAL.Data
             //Add Hornets
             Hornet h1 = new Hornet()
             {
-                //Id = 1,
                 ColorId = 1
             };
 
             Hornet h2 = new Hornet()
             {
-                //Id = 2,
                 ColorId = 2
+            };
+
+            Hornet h3 = new Hornet()
+            {
+                ColorId = 3
+            };
+
+            Hornet h4 = new Hornet()
+            {
+                ColorId = 4
             };
 
             context.Add(h1);
             context.Add(h2);
+            context.Add(h3);
+            context.Add(h4);
 
             context.SaveChanges();
-
-
-            //Add Users
-            User u1 = new User()
-            {
-                //Id = 1,
-                UserSubTag = "User1",
-            };
-
-            User u2 = new User()
-            {
-                //Id = 2,
-                UserSubTag = "User2",
-            };
-
-            context.Add(u1);
-            context.Add(u2);
-
-            //Add UserBeehives
-            UserBeehive ub1 = new UserBeehive()
-            {
-                //Id = 1,
-                BeehiveId = 1,
-                UserId = 1
-            };
-
-            UserBeehive ub2 = new UserBeehive()
-            {
-                //Id = 2,
-                BeehiveId = 2,
-                UserId = 2,
-            };
-
-            context.Add(ub1);
-            context.Add(ub2);
 
             //Add NestLocations
             EstimatedNestLocation nl1 = new EstimatedNestLocation()
@@ -170,42 +140,6 @@ namespace beeInnovative.DAL.Data
             };
             context.Add(nl1);
             context.Add(nl2);
-
-            context.SaveChanges();
-
-            NestLocation dnl1 = new NestLocation()
-            {
-                //Id = 1,
-                Latitude = 51.16021787106364,
-                Longitude = 4.981450191825334,
-                StatusId = 1
-            };
-            NestLocation dnl2 = new NestLocation()
-            {
-                //Id = 2,
-                Latitude = 51.17820318928493,
-                Longitude = 4.996249362951717,
-                StatusId = 2
-            };
-            NestLocation dnl3 = new NestLocation()
-            {
-                //Id = 3,
-                Latitude = 51.18178282293114,
-                Longitude = 4.954444107376311,
-                StatusId = 3
-            };
-            NestLocation dnl4 = new NestLocation()
-            {
-                //Id = 4,
-                Latitude = 51.14696312726804,
-                Longitude = 4.969484197168675,
-                StatusId = 1
-            };
-
-            context.Add(dnl1);
-            context.Add(dnl2);
-            context.Add(dnl3);
-            context.Add(dnl4);
 
             context.SaveChanges();
         }
